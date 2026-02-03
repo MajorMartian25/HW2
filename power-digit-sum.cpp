44e30fd0f6bb989ccd5e45a7e0beac67e7b97511
Josh Freedman <josh.freedman06@gmail.com>
2026-02-03 15:56:57 -0500

    Untested from laptop, with the functions from part two

--- power-digit-sum.cpp
+++ power-digit-sum.cpp
@@ -1,5 +1,5 @@
 #include <iostream> //provides cout
-#include <climits> //used to limit number of cases with overflow problems
+#include <cmath> //used to limit number of cases with overflow problems
 #include <string>
 using namespace std; //skips a lotta syntax
 
@@ -23,36 +23,62 @@ int main() {
             cout << "Input must be a positive integer" << endl;
         }
     }
-    unsigned int in1Int = stoi(in1); //Now that we know they're numbers,
+    unsigned int in1Int = stoi(in1); //Now that we know they're positive, whole numbers,
     unsigned int in2Int = stoi(in2); //make them be ints
     if(validInput){ //This if is to skip unnecessary checks
         //verify the numbers aren't too large
-        if(in1Int > SHRT_MAX || in2Int > SHRT_MAX){
+        if(in2Int*log10(in1Int) >= 64*log10(2)){
             validInput = false;
             cout << "Input too large" << endl;
         }
     }
     if(validInput){
-        unsigned long pow = stol(in1);
-        
-        if(in2Int == 0){
-            pow = 1;
-        }
-        else{
-            for(int i = 0; i < in2Int-1; i++){
-                pow *= in1Int;
-            }
-        }
-        unsigned long toPrint = pow; //so I can change pow in the next part
-        int sumPow = 0;
-        string powString = to_string(pow); //seemed the easiest way to get the length, did have to google
-        for(int i = 0; i < powString.length(); i++){
-            int toAdd = pow%10;
-            pow /= 10;
-            //cout << toAdd << endl; twas testing
-            sumPow += toAdd;
-        }
-        cout << in1 << "^" << in2 << " = " << toPrint << endl << "Sum of Digits: " << sumPow << endl;
+        unsigned long long pow = power(in1Int, in2Int);
+        int sumPow = sum_vector(vectorize_digits(pow));
+        cout << in1 << "^" << in2 << " = " << pow << endl << "Sum of Digits: " << sumPow << endl;
     }
     return 0;
+}
+
+unsigned long long int power(unsigned int a, unsigned int b){
+    unsigned long long pow = a;
+    if(b == 0){
+        pow = 1;
+    }
+    else{
+        for(int i = 0; i < in2Int-1; i++){
+            pow *= a;
+        }
+    }
+    return pow;
+}
+
+vector<int> vectorize_digits(unsigned long long n){
+    vector<int> tR;
+    for(int i = 0; i < powString.length(); i++){
+        int curr = n%10;
+        n /= 10;
+        tR.push_back(curr);
+    }
+    return tR;
+}
+
+int sum_vector(vector<int> v){
+    int sum = 0;
+    for(int i: v){
+        sum += i;
+    }
+    return sum;
+}
+
+string vec_to_string(vector<int> vec){
+    string tR = "[";
+    for(int i = 0; i<vec.size(); i++){
+        tr += to_string(vec[i]);
+        if(i != vec.size()-1){
+            tr += ", ";
+        }
+    }
+    tr += "]";
+    return tR;
 }
